443f8f8101d4d3950c9eb73fa2dde01e3943ae5b
Mandy Meindersma <meinders@ualberta.ca>
2019-08-14 23:35:27 -0600

    adding the length width and height working, getting a better make model ui, added vin and year, need to change the way vin, year and stk look like cause they aren't as pretty as they could be

--- src/Components/InternalForm/InternalForm.js
+++ src/Components/InternalForm/InternalForm.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { Form, Button, Input, Select, Row, Col, InputNumber } from "antd";
-import makeModelConfig from "./MakeModelConfig.js"
+import { Form, Button, Input, InputNumber, Cascader } from "antd";
+import makeModelConfigCascade from "./MakeModelConfig.js"
 
 class InternalForm extends React.Component {
-
-  state = {
-    modelOptions: null
-  }
 
   handleSubmit = e => {
     e.preventDefault();
@@ -20,21 +16,22 @@ class InternalForm extends React.Component {
         ...fieldsValue,
         "first-name": fieldsValue["first-name"],
         "last-name": fieldsValue["last-name"],
-        "make": fieldsValue["make"],
-        "model": fieldsValue["model"],
+        "makeModel": fieldsValue["makeModelCascade"],
         "stk": fieldsValue["stk"],
-        "part-description": fieldsValue["part-description"]
+        "vin": fieldsValue["vin"],
+        "year": fieldsValue["year"],
+        "part-description": fieldsValue["part-description"],
+        "quantity": fieldsValue["quantity"],
+        "lengthFeet": fieldsValue["lengthFeet"],
+        "lengthInches": fieldsValue["lengthInches"],
+        "widthFeet": fieldsValue["widthFeet"],
+        "widthInches": fieldsValue["widthInches"],
+        "heightFeet": fieldsValue["heightFeet"],
+        "heightInches": fieldsValue["heightInches"],
       };
       console.log("Received values of form: ", values);
     });
   };
-
-  handleMakeSelectChange = make => {
-    var modelOptions = makeModelConfig[make].map(model => (
-      <Select.Option value={model} key={model}>{model}</Select.Option>
-    ))
-    this.setState({modelOptions});
-  }
 
   render() {
     const { getFieldDecorator } = this.props.form;
@@ -60,33 +57,18 @@ class InternalForm extends React.Component {
         <Form.Item label="Last Name:">
           {getFieldDecorator("last-name", requiredConfig)(<Input placeholder="Doe" style={{width: "200px"}} />)}
         </Form.Item>
-        <Row type="flex" justify="center">
-          <Col span = {6}>
-            <Form.Item>
-              {getFieldDecorator("make", requiredConfig)(
-                <Select placeholder="Make" onChange={this.handleMakeSelectChange}>
-                  {Object.keys(makeModelConfig).map(make => (
-                    <Select.Option value={make} key={make}>{make}</Select.Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-          </Col>
-          <Col span= {6}>
-            <Form.Item>
-              {getFieldDecorator("model", requiredConfig)(
-                <Select placeholder="Model">
-                  {this.state.modelOptions}
-                </Select>
-              )}
-            </Form.Item>
-          </Col>
-          <Col span = {6}>
-            <Form.Item>
-              {getFieldDecorator("stk", requiredConfig)(<InputNumber min={1000} placeholder="stk #"/>)}
-            </Form.Item>
-          </Col>
-        </Row>
+        <Form.Item label="Make and Model:">
+          {getFieldDecorator('makeModelCascade', requiredConfig)(<Cascader options={makeModelConfigCascade} style={{width: "300px"}}/>)}
+        </Form.Item>
+        <Form.Item label="stk:">
+          {getFieldDecorator("stk", requiredConfig)(<InputNumber min={1000} placeholder="stk #"/>)}
+        </Form.Item>
+        <Form.Item label="VIN">
+          {getFieldDecorator("vin", requiredConfig)(<InputNumber min={1000} placeholder="vin"/>)}
+        </Form.Item>
+        <Form.Item label="Year:">
+          {getFieldDecorator("year", requiredConfig)(<InputNumber min={1000} placeholder="year"/>)}
+        </Form.Item>
         <hr />
         <h1> Tech </h1>
         <Form.Item label="Part Description:">
@@ -96,6 +78,46 @@ class InternalForm extends React.Component {
               rows={4}
               style={{width: "400px"}} />
           )}
+        </Form.Item>
+        <Form.Item label="Quantity:">
+          {getFieldDecorator("quantity", requiredConfig)(<InputNumber min={1} placeholder="quantity"/>)}
+        </Form.Item>
+        <Form.Item label="Length:" style={{ marginBottom: 0 }}>
+          <Form.Item
+            style={{ display: 'inline-block'}}
+          >
+            {getFieldDecorator("lengthFeet", requiredConfig)(<InputNumber min={0} placeholder="feet"/>)}
+            <span className="ant-form-text"> ft</span>
+          </Form.Item>
+          <Form.Item
+            style={{ display: 'inline-block' }}>
+            {getFieldDecorator("lengthInches", requiredConfig)(<InputNumber min={0} max={12} placeholder="inches"/>)}
+            <span className="ant-form-text" style={{ position: "absolute", top: "-10px", left: "90px" }}> in</span>
+          </Form.Item>
+        </Form.Item>
+        <Form.Item label="Width:" style={{ marginBottom: 0 }}>
+          <Form.Item
+            style={{ display: 'inline-block'}}>
+            {getFieldDecorator("widthFeet", requiredConfig)(<InputNumber min={0} placeholder="feet"/>)}
+            <span className="ant-form-text"> ft</span>
+          </Form.Item>
+          <Form.Item
+            style={{ display: 'inline-block' }}>
+            {getFieldDecorator("widthInches", requiredConfig)(<InputNumber min={0} max={12} placeholder="inches"/>)}
+            <span className="ant-form-text" style={{ position: "absolute", top: "-10px", left: "90px" }}> in</span>
+          </Form.Item>
+        </Form.Item>
+        <Form.Item label="Height:" style={{ marginBottom: 0 }}>
+          <Form.Item
+            style={{ display: 'inline-block'}}>
+            {getFieldDecorator("heightFeet", requiredConfig)(<InputNumber min={0} placeholder="feet"/>)}
+            <span className="ant-form-text"> ft</span>
+          </Form.Item>
+          <Form.Item
+            style={{ display: 'inline-block' }}>
+            {getFieldDecorator("heightInches", requiredConfig)(<InputNumber min={0} max={12} placeholder="inches"/>)}
+            <span className="ant-form-text" style={{ position: "absolute", top: "-10px", left: "90px" }}> in</span>
+          </Form.Item>
         </Form.Item>
         <Form.Item
           wrapperCol={{
--- src/Components/InternalForm/MakeModelConfig.js
+++ src/Components/InternalForm/MakeModelConfig.js
@@ -1,7 +1,70 @@
-var makeModelConfig = {
-  "American Coach": ["Eagle", "Heritage", "Revolution", "Allegiance", "Dream", "Tradition"],
-  "Coachman": ["Cross Country", "Cross Country SRS", "Mirada", "Mirada Select", "Pursuit"],
-  "Cool Amphibious": ["Terra Wind"]
-}
+const makeModelConfigCascade = [
+  {
+    value: "American Coach",
+    label: "American Coach",
+    children: [
+      {
+        value: "Eagle",
+        label: "Eagle",
+      },
+      {
+        value: "Heritage",
+        label: "Heritage",
+      },
+      {
+        value: "Revolution",
+        label: "Revolution",
+      },
+      {
+        value: "Allegiance",
+        label: "Allegiance",
+      },
+      {
+        value: "Dream",
+        label: "Dream",
+      },
+      {
+        value: "Tradition",
+        label: "Tradition",
+      },
+    ],
+  },
+  {
+    value: "Coachman",
+    label: "Coachman",
+    children: [
+      {
+        value: "Cross Country",
+        label: "Cross Country"
+      },
+      {
+        value: "Cross Country SRS",
+        label: "Cross Country SRS",
+      },
+      {
+        value: "Mirada",
+        label: "Mirada",
+      },
+      {
+        value: "Mirada Select",
+        label: "Mirada Select",
+      },
+      {
+        value: "Pursuit",
+        label: "Pursuit",
+      },
+    ],
+  },
+  {
+    value: "Cool Amphibious",
+    label: "Cool Amphibious",
+    children: [
+      {
+        value: "Terra Wind",
+        label: "Terra Wind"
+      },
+    ],
+  },
+];
 
-export default makeModelConfig
+export default makeModelConfigCascade
